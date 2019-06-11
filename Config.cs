4901cbcd6d107e9f739122ddfc0b8252f2bd9ac4
hamstar0 <webutek@gmail.com>
2019-06-10 19:36:47 -0700

    v0.7.0 * Finished defining presets for rare items via config

--- Config.cs
+++ Config.cs
@@ -1,5 +1,8 @@
 ﻿using HamstarHelpers.Components.Config;
+using HamstarHelpers.Helpers.ItemHelpers;
 using System;
+using System.Collections.Generic;
+using Terraria.ID;
 
 
 namespace Intrinsics {
@@ -22,11 +25,67 @@ namespace Intrinsics {
 		public int TradeUIPositionX = 64;
 		public int TradeUIPositionY = 256;
 
+		public IDictionary<string, int> TradeItemContractTatters = new Dictionary<string, int>();
+
 
 
 		////////////////
 
 		public void SetDefaults() {
+			string n( int itemId ) => ItemIdentityHelpers.GetProperUniqueId( itemId );
+			
+			 this.TradeItemContractTatters[ n(ItemID.GPS) ] = 1;
+			 this.TradeItemContractTatters[ n(ItemID.REK) ] = 1;
+			 this.TradeItemContractTatters[ n(ItemID.GoblinTech) ] = 1;
+			 this.TradeItemContractTatters[ n(ItemID.ArmorBracing ) ] = 1;
+			 this.TradeItemContractTatters[ n(ItemID.MedicatedBandage ) ] = 1;
+			 this.TradeItemContractTatters[ n(ItemID.CountercurseMantra ) ] = 1;
+			 this.TradeItemContractTatters[ n(ItemID.ThePlan ) ] = 1;
+			this.TradeItemContractTatters[ n(ItemID.AngelStatue) ] = 1;
+
+			 this.TradeItemContractTatters[ n(ItemID.LavaCharm) ] = 2;
+			 this.TradeItemContractTatters[ n(ItemID.WaterWalkingBoots) ] = 2;
+			 this.TradeItemContractTatters[ n(ItemID.JellyfishDivingGear) ] = 2;
+			 this.TradeItemContractTatters[ n(ItemID.SandstorminaBalloon) ] = 2;
+			 this.TradeItemContractTatters[ n(ItemID.ArchitectGizmoPack) ] = 2;
+			 this.TradeItemContractTatters[ n(ItemID.TigerClimbingGear) ] = 2;
+			 this.TradeItemContractTatters[ n(ItemID.MechanicalGlove) ] = 2;
+			 this.TradeItemContractTatters[ n(ItemID.FishFinder) ] = 2;
+			 this.TradeItemContractTatters[ n(ItemID.GoldRing) ] = 2;
+			 this.TradeItemContractTatters[ n(ItemID.LuckyCoin) ] = 2;
+			 this.TradeItemContractTatters[ n(ItemID.DiscountCard) ] = 2;
+			this.TradeItemContractTatters[ n(ItemID.HandWarmer) ] = 2;
+			this.TradeItemContractTatters[ n(ItemID.PocketMirror) ] = 2;
+			this.TradeItemContractTatters[ n(ItemID.CorruptionKey) ] = 2;
+			this.TradeItemContractTatters[ n(ItemID.CrimsonKey) ] = 2;
+			this.TradeItemContractTatters[ n(ItemID.FrozenKey) ] = 2;
+			this.TradeItemContractTatters[ n(ItemID.HallowedKey) ] = 2;
+			this.TradeItemContractTatters[ n(ItemID.JungleKey) ] = 2;
+
+			 this.TradeItemContractTatters[n( ItemID.LightningBoots )] = 3;
+			this.TradeItemContractTatters[ n(ItemID.ArcticDivingGear) ] = 3;
+			this.TradeItemContractTatters[n( ItemID.MasterNinjaGear )] = 3;
+			this.TradeItemContractTatters[n( ItemID.TerraBlade )] = 3;
+			this.TradeItemContractTatters[ n(ItemID.FireGauntlet) ] = 3;
+			this.TradeItemContractTatters[ n(ItemID.FrozenTurtleShell) ] = 3;
+			this.TradeItemContractTatters[ n(ItemID.Uzi) ] = 3;
+			this.TradeItemContractTatters[ n(ItemID.CoinRing) ] = 3;
+			
+			 this.TradeItemContractTatters[ n(ItemID.AnkhCharm ) ] = 4;
+			this.TradeItemContractTatters[ n(ItemID.LavaWaders) ] = 4;
+			this.TradeItemContractTatters[ n(ItemID.BundleofBalloons) ] = 4;
+			this.TradeItemContractTatters[ n(ItemID.FrostsparkBoots) ] = 4;
+			this.TradeItemContractTatters[ n(ItemID.RodofDiscord) ] = 4;
+			
+			 this.TradeItemContractTatters[ n(ItemID.PDA) ] = 5;
+			this.TradeItemContractTatters[n( ItemID.AnkhShield )] = 5;
+			this.TradeItemContractTatters[ n(ItemID.AmberMosquito) ] = 5;
+			this.TradeItemContractTatters[ n(ItemID.CoinGun) ] = 5;
+			this.TradeItemContractTatters[ n(ItemID.AnglerTackleBag) ] = 5;
+
+			this.TradeItemContractTatters[ n(ItemID.CellPhone) ] = 6;
+
+			this.TradeItemContractTatters[ n(ItemID.GreedyRing) ] = 7;
 		}
 
 
